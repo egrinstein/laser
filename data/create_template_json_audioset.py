d7cafcbd6d2971429c9fd965c655bdccfc68a8eb
Eric Grinstein <eric.grinstein@outlook.com>
2024-03-21 11:45:04 +0000

    Improve query augmentation and add test split code

--- data/create_template_json_audioset.py
+++ data/create_template_json_audioset.py
@@ -15,14 +15,16 @@ import os
 import json
 import argparse
 import random
-import numpy as np
-import pandas as pd
 from tqdm import tqdm
 
 MIN_SIZE_IN_BYTES = 5000
 
 
-def create_template_json_audioset(data_dir, output_json):
+def create_template_json_audioset(data_dir, output_json,
+                                  split_train=0.8, split_val=0, split_test=0.2):
+    if split_test + split_val + split_train != 1:
+        raise ValueError("Split values should sum to 1")
+    
     data = []
     for label in tqdm(os.listdir(data_dir)):
         label_dir = os.path.join(data_dir, label)
@@ -38,9 +40,26 @@ def create_template_json_audioset(data_dir, output_json):
                 "caption": label
             })
     random.shuffle(data)
-    json_data = {"data": data}
-    with open(output_json, "w") as f:
-        json.dump(json_data, f, indent=4)
+
+    # Split data
+    data_len = len(data)
+    train_data = data[:int(split_train * data_len)]
+    val_data = data[int(split_train * data_len):int((split_train + split_val) * data_len)]
+    test_data = data[int((split_train + split_val) * data_len):]
+
+    data_dict = {
+        "train": train_data,
+        "val": val_data,
+        "test": test_data
+    }
+
+    for split in data_dict:
+        split_data = data_dict[split]
+        
+        json_data = {"data": split_data}
+        with open(output_json.replace(".json", f"_{split}.json"), "w") as f:
+            json.dump(json_data, f, indent=4)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create a template json file for the AudioSet dataset")
--- query_augmentation/__init__.py
+++ query_augmentation/__init__.py
@@ -21,26 +21,36 @@ MIXED_QUERIES = []
 # TODO: do the inverse as well (negative first)
 for positive_query in POSITIVE_QUERIES:
     for negative_query in NEGATIVE_QUERIES:
-        connectors = ["and ", ", "]
+        connectors = [" and ", ", "]
         connector = random.choice(connectors)
         MIXED_QUERIES.append(positive_query + connector + negative_query.lower())
 
 DESCIPTIVE_QUERIES = [
     "An excerpt of {}",
     "A clip of {}",
-    "This is an audio of {}"
-    "This clip playing an audio of {}"
+    "This is an audio of {}",
+    "A clip playing an audio of {}"
 ]
 
 COMMAND_TYPES = [
     "positive",
-    "descriptive"
-    # "negative",
-    # "mixed",
+    "descriptive",
+    "negative",
+    "mixed",
 ]
 
 
-def caption_to_random_command(caption, interferer_captions):
+def caption_to_random_command(caption: str, interferer_captions: list[str]):
+    # Sometimes AudioSet's captions have synonyms separated by commas.
+    # For example, one class is Accelerating, revving, vroom
+    # We can split these and choose one randomly.
+    def _parse_text(text: str):
+        return random.choice(text.split(",")).lower().strip()
+    
+    caption = _parse_text(caption)
+    interferer_captions = [
+        _parse_text(cap) for cap in interferer_captions]
+
     command_type = random.choice(COMMAND_TYPES)
     if command_type == "positive":
         query = random.choice(POSITIVE_QUERIES).format(caption)
@@ -52,6 +62,8 @@ def caption_to_random_command(caption, interferer_captions):
     elif command_type == "mixed":
         query = random.choice(MIXED_QUERIES).format(
             caption, interferer_captions[0])
+    else:
+        raise ValueError("Invalid command type")
 
     return query
 
